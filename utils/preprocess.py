04f13508f1835534abfc23bb47309764ab0f2ab7
BrunoRaphaell <brunoraphaell99@gmail.com>
2023-02-09 12:08:09 -0300

    so pra passar

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -20,8 +20,44 @@ def remove_duplicate_values(data):
     return data.drop_duplicates()
 
 def modifying_schema(data):
-    with open("dados/schema_creditcard.json") as f:
-        schema = json.load(f)
+    
+    schema = """{
+                    "Amount": "float",
+                    "Class": "int",
+                    "Time": "int",
+                    "V1": "float",
+                    "V10": "float",
+                    "V11": "float",
+                    "V12": "float",
+                    "V13": "float",
+                    "V14": "float",
+                    "V15": "float",
+                    "V16": "float",
+                    "V17": "float",
+                    "V18": "float",
+                    "V19": "float",
+                    "V2": "float",
+                    "V20": "float",
+                    "V21": "float",
+                    "V22": "float",
+                    "V23": "float",
+                    "V24": "float",
+                    "V25": "float",
+                    "V26": "float",
+                    "V27": "float",
+                    "V28": "float",
+                    "V3": "float",
+                    "V4": "float",
+                    "V5": "float",
+                    "V6": "float",
+                    "V7": "float",
+                    "V8": "float",
+                    "V9": "float"
+                }
+            """
+                        
+    # with open("dados/schema_creditcard.json") as f:
+    #     schema = json.load(f)
         
     
     for col, dtype in schema.items():
